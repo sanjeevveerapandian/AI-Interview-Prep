9157cdbc5b7ed4ad4206a1bab50cedd0acdf4280
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2025-04-16 02:20:00 +0530

    sign-up & sign-in implementing of auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,15 @@
-import { ReactNode } from "react"
+import { ReactNode } from 'react'
+import {isAuthenticated} from "@/lib/actions/auth.action";
+import {redirect} from "next/navigation";
 
-const AuthLayout = ({children}:{children:React.ReactNode}) => {
-  return (
-    <div className="auth-layout">{children}</div>
-  )
+const AuthLayout = async ({ children }: { children: ReactNode  }) => {
+    const isUserAuthenticated = await isAuthenticated();
+
+    if(isUserAuthenticated) redirect('/');
+
+    return (
+        <div className="auth-layout">{children}</div>
+    )
 }
 
 export default AuthLayout
