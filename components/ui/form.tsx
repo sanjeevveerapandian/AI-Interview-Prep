00341fa50d43b7e9449b61b70c70218ce9ddaf45
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2025-04-19 09:51:30 +0530

    commit

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -14,7 +14,7 @@ import {
 } from "react-hook-form";
 
 import { cn } from "@/lib/utils";
-import { Label } from "@/app/components/ui/label";
+import { Label } from "./label";
 
 const Form = FormProvider;
 
